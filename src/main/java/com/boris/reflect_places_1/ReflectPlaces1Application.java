58b773989c302cdb097a871e887abd8d98b7ada8
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2024-09-11 00:34:00 +0400

    1.18

--- src/main/java/com/boris/reflect_places_1/ReflectPlaces1Application.java
+++ src/main/java/com/boris/reflect_places_1/ReflectPlaces1Application.java
@@ -2,10 +2,10 @@ package com.boris.reflect_places_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ReflectPlaces1Application {
 
 	public static void main(String[] args) {
