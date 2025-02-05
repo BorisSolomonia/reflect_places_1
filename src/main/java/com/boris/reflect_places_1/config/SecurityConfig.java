e330d7c007784648b7e3431064b1056f5895995d
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2025-02-06 00:17:38 +0400

    change reactive

--- src/main/java/com/boris/reflect_places_1/config/SecurityConfig.java
+++ src/main/java/com/boris/reflect_places_1/config/SecurityConfig.java
@@ -11,8 +11,9 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 import java.util.Arrays;
 
