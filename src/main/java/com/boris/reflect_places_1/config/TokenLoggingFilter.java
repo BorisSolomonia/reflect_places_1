a612f29ffc8052d88f53bfe5faaff7cc43b11ea5
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2025-02-20 02:26:26 +0400

    https://www.brooks-dusura.uk/api

--- src/main/java/com/boris/reflect_places_1/config/TokenLoggingFilter.java
+++ src/main/java/com/boris/reflect_places_1/config/TokenLoggingFilter.java
@@ -31,7 +31,7 @@ public class TokenLoggingFilter extends OncePerRequestFilter {
             try {
                 Jwt jwt = jwtDecoder.decode(token);
                 logger.info("✅ Valid Token Received: {}", jwt.getClaims());
-                logger.info(jwt.getClaims().containsValue("write:places") + "True Boris");
+                logger.info(jwt.getClaims().containsValue("https://www.brooks-dusura.uk/api") + "True Boris");
             } catch (Exception e) {
                 logger.error("🚨 Invalid Token: {}", e.getMessage());
             }
